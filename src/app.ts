432b3823f3ee489305410626a05af456ef1a79e9
Aasim-Qureshi <aasimq194@gmail.com>
2025-07-10 15:13:52 +0530

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,13 @@ dotenv.config();
 const app: Express = express();
 
 app.use(cookieParser());
-app.use(cors());
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(',') || '*',
+        credentials: true,
+}));
+
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
