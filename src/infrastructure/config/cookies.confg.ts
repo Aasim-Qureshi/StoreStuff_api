10cc98311296805bf2317407ab85441f02e81cda
Aasim-Qureshi <aasimq194@gmail.com>
2025-07-11 16:13:12 +0530

    Fixing Cors

--- src/infrastructure/config/cookies.confg.ts
+++ src/infrastructure/config/cookies.confg.ts
@@ -1,7 +1,8 @@
 import { CookieOptions } from "express";
 
 export const cookieConfig: CookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production", 
-    sameSite: "lax", 
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", 
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
 };
+
